1bd0a5018c6794f2f74cd08fbc465fbc68ac14d2
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-05-22 23:02:06 +0900

    🚀 init commit

---
+++ app/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'postgres',
+  database: 'postgres_db',
+  password: 'password',
+  port: 5432,
+})
+
+app.get('/', async(req, res) => {
+  const { rows } = await pool.query('select * from users')
+  res.send(rows)
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+}); 
+
+'use strict';
